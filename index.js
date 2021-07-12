d932f4fe71afb01a330e5735d7d5f933913b0823
Alison Parisaca <alison-pq@outlook.com>
2021-07-11 20:48:49 -0400

    Getting IP and Hostname

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const os = require("os");
 
 app.get('/api/get-health', (req, res) => {
   const today = new Date();
+  var forwardedIpsStr = req.header('x-forwarded-for');
   res.send({
     time: `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`,
     date: `${today.getDate()}/${today.getMonth()}/${today.getFullYear()}`,
+    IP: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+    hostname:os.hostname(),
   });
 });
 
